2c1e295a50245f181955d122c01b792cfc2ddb38
osully1 <owensullivan482@gmail.com>
2021-07-27 09:09:47 -0400

    Made GameTable.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { newGameDeck } from './services/card-api';
+import GameTable from './components/GameTable/GameTable';
 
 function App() {
+
+  const [ deckData, setDeckData ] = useState({})
+  const [ scores, setScores ] = useState([])
+
+  async function newGame() {
+    const data = await newGameDeck()
+    setDeckSata(data)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
+        <GameTable
+        
+        />
     </div>
   );
 }
---
+++ src/components/GameTable/GameTable.js
@@ -0,0 +1,11 @@
+import { useState, useRef, useEffect } from 'react';
+import { drawCardsP1 } from '../../services/card-api';
+import { drawCardsP2 } from '../../services/card-api';
+import { drawCommonCards } from '../../services/card-api';
+import styles from './GameTable.module.css';
+
+const GameTable = (props) => {
+
+}
+
+export default GameTable;
