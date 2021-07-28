540b0e30bbcd5519f5f69028745244a89eb1813d
osully1 <owensullivan482@gmail.com>
2021-07-28 07:37:06 -0400

    got card selections to work

--- src/components/PlayArea/PlayArea.js
+++ src/components/PlayArea/PlayArea.js
@@ -19,11 +19,46 @@ const PlayArea = (props) => {
         }
     })
 
+    function toggleCardP1(e) {
+        let newArr1 = props.p1Tally.cCardValue.concat(e)
+        props.setP1Tally((prevState) => ({
+            ...prevState,
+            cCardValue: newArr1
+        }))
+    }
+
+    function toggleCardP2(e) {
+        let newArr2 = props.p2Tally.cCardValue.concat(e)
+        props.setP2Tally((prevState) => ({
+            ...prevState,
+            cCardValue: newArr2
+        }))
+    }
+
     return props.commonCards.map((card, idx) => {
         return(
             <button 
                 className={css(styles.CommonCards)}
                 style={{backgroundImage: "url(" + `${card.image}` + ")"}}
+                onClick={() => {
+                    if (
+                        props.p1Tally.pCardValue
+                        && props.turn === true
+                        && props.p1Tally.cCardValue.length < 4
+                        && props.p1Tally.cCardValue.every(function (i) {
+                            return i.code !== card.code
+                        })) {
+                        toggleCardP1(card)
+                    } else if (
+                        props.p2Tally.pCardValue
+                        && props.turn === false
+                        && props.p2Tally.cCardValue.length < 4
+                        && props.p2Tally.cCardValue.every(function (i) {
+                            return i.code !== card.code
+                        })) {
+                        toggleCardP2(card)
+                    }
+                }}
                 key={idx}
             />
         )
