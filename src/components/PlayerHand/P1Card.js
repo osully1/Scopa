054a3d49ad2bdec14d904d9cb4e6515c5c02dc7b
osully1 <owensullivan482@gmail.com>
2021-07-28 13:31:31 -0400

    select transform works. Going to put card components back into p1hand

---
+++ src/components/PlayerHand/P1Card.js
@@ -0,0 +1,49 @@
+import { slideInDown, bounce } from 'react-animations'
+import { useState, useEffect } from 'react';
+import { css, StyleSheet } from "aphrodite"
+
+const P1Card = (props) => {
+
+    const styles = StyleSheet.create({
+        P1Hand: {
+            animationName: slideInDown,
+            animationDuration: '1s',
+            margin: '1rem',
+        },
+        P1HandActive: {
+            animationName: slideInDown,
+            animationDuration: '1s',
+            margin: '1rem',
+            transform: 'translateY(-10px)'
+        }
+    })
+
+
+    function toggleCard(e) {
+        props.setP1Tally({pCardValue: e, cCardValue: []})
+    }
+    
+    return(
+        <div className={css([styles.P1Hand, props.p1Tally.pCardValue.code === props.card.code && styles.P1HandActive])} key={props.idx}>
+            <button 
+                style={{
+                    height: '7em',
+                    width: '5em',
+                    margin: '1em',
+                    backgroundImage: "url(" + `${props.card.image}` + ")",
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no - repeat',
+                    backgroundSize: 'cover',
+                }}
+                onClick={() => {
+                    if (props.turn === true) {
+                        toggleCard(props.card)
+                    }
+                }}
+            />
+        </div>
+    )
+
+}
+
+export default P1Card
--- src/components/PlayerHand/P1Hand.js
+++ src/components/PlayerHand/P1Hand.js
@@ -1,51 +1,42 @@
 import { slideInDown, bounce } from 'react-animations'
 import { useState, useEffect } from 'react';
 import { css, StyleSheet } from "aphrodite"
+import P1Card from './P1Card'
 
 const P1Hand = (props) => {
 
-    const styles = StyleSheet.create({
-        P1Hand: {
-            animationName: slideInDown,
-            animationDuration: '1s',
-            margin: '1rem',
-        },
-        P1HandActive: {
-            animationName: slideInDown,
-            animationDuration: '1s',
-            margin: '1rem',
-            ':active': {
-                transform: 'translateY(-10px)'
-            }
-        }
-    })
+    // const styles = StyleSheet.create({
+    //     P1Hand: {
+    //         animationName: slideInDown,
+    //         animationDuration: '1s',
+    //         margin: '1rem',
+    //     },
+    //     P1HandActive: {
+    //         animationName: slideInDown,
+    //         animationDuration: '1s',
+    //         margin: '1rem',
+    //         ':active': {
+    //             transform: 'translateY(-10px)'
+    //         }
+    //     }
+    // })
 
-    const [ turnState, setTurnState ] = useState(styles.P1HandActive)
-
-    function toggleCard(e) {
-        props.setP1Tally({pCardValue: e, cCardValue: []})
-    }
+    // function toggleCard(e) {
+    //     props.setP1Tally({pCardValue: e, cCardValue: []})
+    // }
 
     return props.p1Hand.map((card, idx) => {
         return(
-            <div className={css(turnState)} key={idx}>
-                <button 
-                    style={{
-                        height: '7em',
-                        width: '5em',
-                        margin: '1em',
-                        backgroundImage: "url(" + `${card.image}` + ")",
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no - repeat',
-                        backgroundSize: 'cover',
-                    }}
-                    onClick={() => {
-                        if (props.turn === true) {
-                            toggleCard(card)
-                        }
-                    }}
-                />
-            </div>
+            <P1Card
+                card={card}
+                idx={idx}
+                p1Hand={props.p1Hand}
+                setP1Hand={props.setP1Hand}
+                p1Tally={props.p1Tally}
+                setP1Tally={props.setP1Tally}
+                turn={props.turn}
+                setTurn={props.setTurn}
+            />
         )
     })
 }
