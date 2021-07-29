428740b1f3b6e4ab2dc2b9680929f135fe6e3b45
osully1 <owensullivan482@gmail.com>
2021-07-29 08:44:58 -0400

    Operation was a success

--- src/components/PlayerHand/P1Hand.js
+++ src/components/PlayerHand/P1Hand.js
@@ -5,38 +5,54 @@ import P1Card from './P1Card'
 
 const P1Hand = (props) => {
 
-    // const styles = StyleSheet.create({
-    //     P1Hand: {
-    //         animationName: slideInDown,
-    //         animationDuration: '1s',
-    //         margin: '1rem',
-    //     },
-    //     P1HandActive: {
-    //         animationName: slideInDown,
-    //         animationDuration: '1s',
-    //         margin: '1rem',
-    //         ':active': {
-    //             transform: 'translateY(-10px)'
-    //         }
-    //     }
-    // })
+    const styles = StyleSheet.create({
+        P1Hand: {
+            animationName: slideInDown,
+            animationDuration: '1s',
+            margin: '1rem',
+        },
+        P1HandActive: {
+            animationName: slideInDown,
+            animationDuration: '1s',
+            margin: '1rem',
+            transform: 'translateY(-10px)'
+        }
+    })
 
-    // function toggleCard(e) {
-    //     props.setP1Tally({pCardValue: e, cCardValue: []})
-    // }
+    function toggleCard(e) {
+        props.setP1Tally({pCardValue: e, cCardValue: []})
+    }
 
     return props.p1Hand.map((card, idx) => {
         return(
-            <P1Card
-                card={card}
-                idx={idx}
-                p1Hand={props.p1Hand}
-                setP1Hand={props.setP1Hand}
-                p1Tally={props.p1Tally}
-                setP1Tally={props.setP1Tally}
-                turn={props.turn}
-                setTurn={props.setTurn}
-            />
+            // <P1Card
+            //     card={card}
+            //     idx={idx}
+            //     p1Hand={props.p1Hand}
+            //     setP1Hand={props.setP1Hand}
+            //     p1Tally={props.p1Tally}
+            //     setP1Tally={props.setP1Tally}
+            //     turn={props.turn}
+            //     setTurn={props.setTurn}
+            // />
+            <div className={css([styles.P1Hand, props.p1Tally.pCardValue.code === card.code && styles.P1HandActive])} key={props.idx}>
+                <button 
+                    style={{
+                        height: '7em',
+                        width: '5em',
+                        margin: '1em',
+                        backgroundImage: "url(" + `${card.image}` + ")",
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no - repeat',
+                        backgroundSize: 'cover',
+                    }}
+                    onClick={() => {
+                        if (props.turn === true) {
+                            toggleCard(card)
+                        }
+                    }}
+                />
+            </div>
         )
     })
 }
