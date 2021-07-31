f6fe26762839986a96bff8ab66c05bdb432bb306
osully1 <owensullivan482@gmail.com>
2021-07-31 17:23:21 -0400

    Worked out styles function to change play card button to active.

--- src/components/PlayButtons/P1PlayButtons.js
+++ src/components/PlayButtons/P1PlayButtons.js
@@ -1,9 +1,25 @@
 import styles from './PlayButtons.module.css'
+import { css, StyleSheet } from "aphrodite"
 
 const P1PlayButtons = (props) => {
+
+    const styles = StyleSheet.create({
+        P1Play: {
+            marginBottom: '10px',
+            pointerEvents: 'none'
+        },
+        P1PlayActive: {
+            marginBottom: '10px',
+            backgroundColor: 'red'
+        }
+    })
+
     return(
         <div className={styles.p1btns}>
-            <button style={{marginBottom: '10px'}}>Play Card</button>
+            <button 
+                // className will change play button to active when value of player card matches the sum of values of selected common cards
+                className={css([styles.P1Play, props.p1Tally.pCardValue.value === props.p1Tally.cCardValue.map(x => x.value).reduce((a, b) => a + b, 0) && styles.P1PlayActive])}
+            >Play Card</button>
             <button>Discard</button>
         </div>
     )
