8d0b1c9071f963434c8a879740881bb92f8dd365
osully1 <owensullivan482@gmail.com>
2021-08-01 16:41:55 -0400

    Now all play and discard buttons work properly and the game runs through each subround. Now need to write functions to detect a round end and determine scores and new round starts

--- src/components/PlayButtons/P1PlayButtons.js
+++ src/components/PlayButtons/P1PlayButtons.js
@@ -45,13 +45,13 @@ const P1PlayButtons = (props) => {
 
     async function newDeal1() {
         const p1Data = await drawCardsP1(props.deckData.deck_id)
-        props.setP1Hand(p1Data)
+        props.setP1Hand(p1Data.cards)
     }
 
     // WARNING!!! MUST REMEMBER THAT SETTING REMAINING STATE IS ASYNCHRONOUS!!! THAT WILL BITE YOU IN THE BUTT WHEN YOU HAVE TO CHECK WHETHER OR NOT THE ROUND IS OVER TO SHUFFLE AND RE-DEAL ALL CARDS!!!!
     async function newDeal2() {
         const p2Data = await drawCardsP2(props.deckData.deck_id)
-        props.setP2Hand(p2Data)
+        props.setP2Hand(p2Data.cards)
         props.setDeckData((prevState) => ({
             ...prevState,
             remaining: props.deckData.remaining -= 6
@@ -66,7 +66,7 @@ const P1PlayButtons = (props) => {
         // Switches turns
         props.setTurn(false)
 
-        // Creates an array of the p1's new hand after discarding p1Tally.pCardValue card. Will check if this array is empty to determine if the round is over
+        // Creates an array of the p1's new hand after discarding pCardValue card. Later, will check if this array is empty to determine if the round is over
         let newHand = []
         props.p1Hand.forEach((card) => {
             if (card.code !== props.p1Tally.pCardValue.code) {
@@ -115,6 +115,44 @@ const P1PlayButtons = (props) => {
         // Need to finish this function. Need to keep in mind that setting state is asynchronous and I must use variables for immediate actions and then set state later in the same button click
     }
 
+    const discardButtonFunction = () => {
+
+        // Switches turns
+        props.setTurn(false)
+
+        // Creates an array of the p1's new hand after discarding pCardValue card. Later, will check if this array is empty to determine if the round is over
+        let newHand = []
+        props.p1Hand.forEach((card) => {
+            if (card.code !== props.p1Tally.pCardValue.code) {
+                newHand.push(card)
+            }
+        })
+        props.setP1Hand(newHand)
+
+        // Puts selected player card into the common cards
+        props.setCommonCards((prevState) => ([
+            ...prevState, props.p1Tally.pCardValue
+        ]))
+
+        // Resets P1Tally so no cards are selected
+        props.setP1Tally({pCardValue: {}, cCardValue: []})
+
+        // Checks if subround is over and new cards need to be dealt, then deals new cards if needbe.
+        if (
+            newHand.length === 0
+            && props.p2Hand.length === 0
+            && props.deckData.remaining > 0
+        ) {
+            setTimeout(() => {
+                newDeal1()   
+            }, 800)
+
+            setTimeout(() => {
+                newDeal2()   
+            }, 1600)
+        }
+    }
+
     return(
         <div className={styles.p1btns}>
             <button 
@@ -124,6 +162,7 @@ const P1PlayButtons = (props) => {
             >Play Card</button>
             <button
                 className={css([stylesb.DiscInactive, props.gameOn === false && stylesb.DiscInvis, Object.keys(props.p1Tally.pCardValue).length && props.p1Tally.cCardValue.length < 1 && stylesb.DiscActive])}
+                onClick={() => discardButtonFunction()}
             >Discard</button>
         </div>
     )
